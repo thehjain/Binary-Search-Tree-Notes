64268d46b520abc7d612d6d8ceaa74175bd9a025
Harsh <thehjain@gmail.com>
2020-11-25 19:40:42 +0530

    Construct BST from given keys

---
+++ ConstructBST.java
@@ -0,0 +1,9 @@
+//Here we have given keys at random and
+//we have to construct binary search tree
+//from that keys.
+
+
+//If we are asked to tell number of all
+//unique bst's that can be build with
+//given keys then it is nothing but catallan
+//number
