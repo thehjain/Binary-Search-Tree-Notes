bad915f3d7d84efd75bfff5dee7e6fa77d5da33d
Harsh <thehjain@gmail.com>
2021-03-18 22:07:02 +0530

    Construct BST from given keys

--- ConstructBST.java
+++ ConstructBST.java
@@ -2,8 +2,32 @@
 //we have to construct binary search tree
 //from that keys.
 
+//Here Array given is sorted
+
+public TreeNode sortedArrayToBST(int[] nums) {
+
+	if (nums.length == 0)
+		return null;
+
+	return helper(nums, 0, nums.length - 1);
+}
+
+TreeNode helper(int[] nums, int low, int high) {
+	if (low > high)
+		return null;
+	int mid = (low + high) / 2;
+	TreeNode curr = new TreeNode(nums[mid]);
+	curr.left = helper(nums, low, mid - 1);
+	curr.right = helper(nums, mid + 1, high);
+	return curr;
+}
+
+
+//Problem is present on leetcode
+
 
 //If we are asked to tell number of all
 //unique bst's that can be build with
 //given keys then it is nothing but catallan
 //number
+
